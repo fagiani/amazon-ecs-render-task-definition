01dde4a4b30ec627ebe0c677bd94c573ff6bf745
Carlos Fagiani Junior <fagianijunior@gmail.com>
2020-12-14 14:29:47 -0300

    Set the correct var

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ async function run() {
         name,
         value
       }));
-      if (AwsSmArns == true) {
-        containerDef.taskRoleArn       = JSON.parse(SecretString).TASK_ROLE_ARN;
-        containerDef.executionRoleArn  = JSON.parse(SecretString).EXECUTION_ROLE_ARN;
+      if (awsSmArns == true) {
+        taskDefContents.taskRoleArn       = JSON.parse(SecretString).TASK_ROLE_ARN;
+        taskDefContents.executionRoleArn  = JSON.parse(SecretString).EXECUTION_ROLE_ARN;
       }
     }
 
